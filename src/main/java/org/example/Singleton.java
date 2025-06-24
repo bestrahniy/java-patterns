14a10e78b529283bff2f046df39b27067f95adc5
Ilya <bobkovilya06@gmail.com>
2025-06-25 01:21:29 +0300

    Create Singleton pattern

---
+++ src/main/java/org/example/Singleton.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public final class Singleton {
+
+    private static Singleton instance;
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null){
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+}
