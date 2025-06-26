d990c1d695db3d66b969022fb34b3b5d6bfaacc8
Ilya <bobkovilya06@gmail.com>
2025-06-26 20:38:30 +0300

    Created a javadock

--- src/main/java/org/example/Singleton.java
+++ src/main/java/org/example/Singleton.java
@@ -1,11 +1,20 @@
 package org.example;
 
+/**
+ * class that defines singleton pattern
+ * this method is thread safe and fast
+ * @author Бобков Илья
+ */
 public final class Singleton {
 
     private static Singleton instance;
 
     private Singleton() {}
 
+    /**
+     * method returns new ar already existing object
+     * @return new or already existing object
+     */
     public static Singleton getInstance() {
         if (instance == null) {
             synchronized (Singleton.class) {
